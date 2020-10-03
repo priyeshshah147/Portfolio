06f5e89eece225425b7fc8076f73092c654b6dff
Harsh Kumar <harsh@hkumar.me>
2020-10-02 19:30:42 -0700

    Header with mobile and tablet styling

---
+++ portfolio/src/components/header/header.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from '../../static/img/logo.png'
+import { useToggle } from '../../utils/hooks';
+
+const Header = () => {
+    const [expanded, toggleExpanded] = useToggle(false);
+
+    return (
+        <header
+            className={expanded ? 'expanded' : ''}
+            aria-expanded={expanded}
+        >
+            <button
+                className="expand-header-button"
+                role="switch"
+                aria-checked={expanded}
+                onClick={toggleExpanded}
+            >
+                <span className="accessible-text">{expanded ? 'Compress header' : 'Expand Header'}</span>
+                <img src="" alt="" aria-hidden/>
+            </button>
+            <section id="name-wrapper">
+                <img src={logo} aria-hidden alt="" />
+                <h1>Harsh Kumar</h1>
+            </section>
+            <nav>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
