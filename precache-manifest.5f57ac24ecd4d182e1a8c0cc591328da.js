98195007d185d6519b06a908576ed8dbac48cf6b
priyeshshah147 <priyesh.shah147@gmail.com>
2021-10-18 10:32:25 -0400

    Updates

---
+++ precache-manifest.5f57ac24ecd4d182e1a8c0cc591328da.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6661bd89fc940580e1943ce3b8f84f02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6486e99a067cc1d1cef2",
+    "url": "/static/css/2.2e126fe7.chunk.css"
+  },
+  {
+    "revision": "a1e6913b83b0d106854f",
+    "url": "/static/css/main.22806afd.chunk.css"
+  },
+  {
+    "revision": "6486e99a067cc1d1cef2",
+    "url": "/static/js/2.156a99ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.156a99ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1e6913b83b0d106854f",
+    "url": "/static/js/main.1ef1c7ab.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "b582aa6e14951ff451aac23399965446",
+    "url": "/static/media/archery_site.b582aa6e.JPG"
+  },
+  {
+    "revision": "003231a1f009db089ab16093ce59e4d2",
+    "url": "/static/media/fa-brands-400.003231a1.eot"
+  },
+  {
+    "revision": "2ec1097c081aa61e17956be4e8008cba",
+    "url": "/static/media/fa-brands-400.2ec1097c.ttf"
+  },
+  {
+    "revision": "3237e6d6e151b80ed2686fedbbb280f4",
+    "url": "/static/media/fa-brands-400.3237e6d6.svg"
+  },
+  {
+    "revision": "5795bc0a3b45e06502187ca104557ca3",
+    "url": "/static/media/fa-brands-400.5795bc0a.woff2"
+  },
+  {
+    "revision": "90cd422fde1f01392a34aaec011a5dda",
+    "url": "/static/media/fa-brands-400.90cd422f.woff"
+  },
+  {
+    "revision": "2788556a1a246f0d0167ce5b4189151e",
+    "url": "/static/media/fa-regular-400.2788556a.svg"
+  },
+  {
+    "revision": "27a4e6fdc8464141b7a957dba093826a",
+    "url": "/static/media/fa-regular-400.27a4e6fd.woff2"
+  },
+  {
+    "revision": "3cefd3390001dcb1eb4629f61bba9185",
+    "url": "/static/media/fa-regular-400.3cefd339.ttf"
+  },
+  {
+    "revision": "8a8d8cd6f4c8a8e556d75eb45b317f29",
+    "url": "/static/media/fa-regular-400.8a8d8cd6.woff"
+  },
+  {
+    "revision": "ee87e7fcb588d700111c24d5ada28775",
+    "url": "/static/media/fa-regular-400.ee87e7fc.eot"
+  },
+  {
+    "revision": "06d5f03672635f0a67010721e8e9929d",
+    "url": "/static/media/fa-solid-900.06d5f036.eot"
+  },
+  {
+    "revision": "2eaaf2a78a47f2d9ae8d1d836eed5d24",
+    "url": "/static/media/fa-solid-900.2eaaf2a7.ttf"
+  },
+  {
+    "revision": "55071ae1abc18726e23174192ebc0b68",
+    "url": "/static/media/fa-solid-900.55071ae1.woff2"
+  },
+  {
+    "revision": "56df75b6af75497c7a9a2b0847981f40",
+    "url": "/static/media/fa-solid-900.56df75b6.woff"
+  },
+  {
+    "revision": "8e39d5d50ead4840070075889f5daeaa",
+    "url": "/static/media/fa-solid-900.8e39d5d5.svg"
+  },
+  {
+    "revision": "4aeaa5e59b91aa13f246f177338cb6e2",
+    "url": "/static/media/getsey_site.4aeaa5e5.JPG"
+  },
+  {
+    "revision": "efab3530020bc20858d12c1691970747",
+    "url": "/static/media/logo1.efab3530.png"
+  },
+  {
+    "revision": "4efcfecaa8d0be4b35daea3e935c9cbd",
+    "url": "/static/media/logo2.4efcfeca.png"
+  },
+  {
+    "revision": "3b3123eb923828299f8382e1e8772b95",
+    "url": "/static/media/menu.3b3123eb.svg"
+  },
+  {
+    "revision": "b6cfa4ff10b56d99d238ca8624897917",
+    "url": "/static/media/resume.b6cfa4ff.pdf"
+  },
+  {
+    "revision": "94409686d42b17c249bda09577d5903e",
+    "url": "/static/media/somashare_site.94409686.JPG"
+  }
+]);
