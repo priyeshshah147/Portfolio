b8411428d1c074e73bf11a73f0606fb40e36c23c
priyeshshah147 <priyesh.shah147@gmail.com>
2021-10-20 20:30:00 -0400

    typo fixes

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -17,7 +17,7 @@ const About = () => {
                     </p>
 
                     <p>
-                        I have recently graduated from App Academy, an intensive 1000hr coding bootcamp where I learnt a greate amount of technologies that enhanced my software engineering skills. 
+                        I have recently graduated from App Academy, an intensive 1000hr coding bootcamp where I learnt a great amount of technologies that enhanced my software engineering skills. 
                     </p>
 
                     <p>Prior to App Academy I graduated with a degree in Mechatronic Engineer, there after I worked as Technical Sales Manager for a machinery retail outlet and I also became a Project Lead for Liter of Light(a NGO that helps provide solar lights in poor areas.</p>
